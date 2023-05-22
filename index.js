d195911dfe5d224fd6a8700f66419e3d2d308c7e
JMontskie <josephmanuelpmonton@gmail.com>
2023-05-23 02:41:16 +0800

    updated frontend

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// Add an event listener to the submit button
+// var submitButton = document.getElementById('submitBtn');
+// submitButton.addEventListener('click', handleSubmit);
+
 function openModal() {
     document.getElementById("projectModal").style.display = "block";
   }
@@ -6,12 +10,15 @@ function closeModal() {
 document.getElementById("projectModal").style.display = "none";
 }
 
-function submitProject() {
+function handleSubmit() {
     // Retrieve input field values and perform desired actions
     const title = document.getElementById("projectTitle").value;
     const description = document.getElementById("projectDescription").value;
     const goalAmount = document.getElementById("goalAmount").value;
     const duration = document.getElementById("duration").value;
+
+    // Generate a random project ID (replace this with your own logic)
+    var projectId = generateProjectId();
   
     // Perform further processing or validation as needed
     console.log("Project submitted:");
@@ -19,11 +26,22 @@ function submitProject() {
     console.log("Description:", description);
     console.log("Goal Amount:", goalAmount);
     console.log("Duration (in days):", duration);
+
+    var message = 'Project created! Your project ID is: ' + projectId;
+    alert(message);
   
     // Reset form and close modal
-    //document.getElementById("projectModal").reset();
-    //closeModal();
+    document.getElementById("projectModalForm").reset();
+    closeModal();
   }
+
+// Function to generate a random project ID
+function generateProjectId() {
+  // Replace this with your own logic to generate a project ID
+  // For simplicity, we'll use a random number in this example
+  var projectId = Math.floor(Math.random() * 1000000);
+  return projectId;
+}
 
 function openExploreModal() {
   document.getElementById("exploreModal").style.display = "block";
@@ -34,7 +52,7 @@ function closeExploreModal() {
 }
 
 function searchProject() {
-  const projectID = document.getElementById("projectID").value;
+  const projectID = document.getElementById("exploreProjectId").value;
   
   // Perform project search and retrieve project details based on the projectID
   // Assuming projectDetails is an example object with project details
@@ -43,6 +61,10 @@ function searchProject() {
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     goalAmount: 1000,
     duration: 30
+  };
+
+  const projectDetails2 = {
+
   };
 
   // Display project details on the main page
@@ -55,6 +77,32 @@ function searchProject() {
   `;
   projectDetailsContainer.style.display = "block";
 
+  // Get the project ID from the input field
+  // var projectId = document.getElementById('exploreProjectId').value;
+
+  // Generate the HTML code for the "Contribute to Project" button
+  var contributeButton = '<button class="contribute-button">Contribute to Project</button>';
+
+  // Append the button to the project-details div
+  var projectDetailsDiv = document.querySelector('.project-details');
+  projectDetailsDiv.innerHTML += contributeButton;
+
+  // Show the footer
+  var footer = document.querySelector('.footer');
+  footer.style.display = 'block';
+
+  // hide the footer-display
+  var footer = document.querySelector('.footer-display');
+  footer.style.display = 'none';
+
+  var walletAddressElement = document.getElementById('walletAddress');
+  // Update the value
+  walletAddressElement.textContent = '0x1234'; // Replace '0x1234' with the actual wallet address value
+
   // Close explore modal
   closeExploreModal();
 }
+
+// footer functions
+var footerPlaceholder = document.querySelector('.footer-placeholder');
+footerPlaceholder.innerHTML = '<i class="fas fa-copyright"></i> FakeKickstarter 2023';
